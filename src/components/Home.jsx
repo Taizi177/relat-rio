0399cef5c67af919ab29cf16d10f94d9ac0b0b92
Taizi <taizisilveira@gmail.com>
2025-04-18 02:38:23 -0300

    adicionei o texto sobre a importancia na pagina Home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,6 +5,8 @@ const Home = () => {
   return (
     <div className="home-content">
       <h1>Relatório 16/04</h1>
+      <p>A Sexta-feira Santa e a Páscoa são celebrações centrais do cristianismo, marcando o sacrifício de Jesus Cristo e sua ressurreição.</p>
+      <p>Celebrá-las é importante porque nos convida à reflexão sobre o amor, a fé, o perdão e a esperança de uma nova vida.</p>
       
       <div className='link'>
         <Link to="/Relatorio">Relatorio Páscoa</Link>
